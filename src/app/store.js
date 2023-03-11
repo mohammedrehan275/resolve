de1f1cba35cb604bb52620fba3dd1b43c60ebd58
mohammedrehan275 <mohammedrehan275@gmail.com>
2023-03-11 14:50:12 +0530

    setting redux store

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import ticketReducer from '../features/ticketSlice';
+
+
+export default configureStore({
+  reducer: {
+    ticket: ticketReducer,
+  },
+});
